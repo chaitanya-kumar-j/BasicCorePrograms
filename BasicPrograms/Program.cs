f8f46516360cdceff8be9ec9c7c37e5b87b5d963
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-21 15:38:47 +0530

    [Chaitanya] Add : Added FlipCoin Problem Program

---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Select the Program number
+            // 1. FlipCoin, 2. CheckLeapYear, 3. PowerOf2Table, 4. HarmonicNumber, 5. PrimeFactorization
+            // 6. QuotientAndRemainder
+            Console.WriteLine("Select and enter the problem number between 1 to 10\n" +
+                "(1. FlipCoin, 2. CheckLeapYear, 3. PowerOf2Table, 4. HarmonicNumber, \n 5. PrimeFactorization," +
+                " 6. QuotientAndRemainder, 7. SwapNumbers,\n 8. EvenOrOdd, 9. VowelOrConsonant, 10. LargestNumber): ");
+            int problemNumber = Convert.ToInt32(Console.ReadLine());
+            switch (problemNumber)
+            {
+                case 1:
+                    var tuple = new PercOccurance().PercentageOccurance();
+                    Console.WriteLine($"Head occurance percentage is: {tuple.Item1}%");
+                    Console.WriteLine($"Tail occurance percentage is: {tuple.Item2}%");
+                    break;
+            }
+        }
+    }
+}

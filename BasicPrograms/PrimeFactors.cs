5839c5678307296f8fd694a0ed26809912333eb2
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-22 07:35:08 +0530

    [Chaitanya] Add : Added Prime Facorization Program

---
+++ BasicPrograms/PrimeFactors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class PrimeFactors
+    {
+        // initialize constants
+        int N = 100;
+        public void Factorizaton()
+        {
+            int n = N, j = 0;
+            while (j < n)
+            {
+                int i = j;
+                if (i == 0 || i == 1)
+                {
+                    i++;
+                    break;
+                }
+                if (i > 1 & n % i != 0)
+                {
+                    while (i < n)
+                    {
+                        if (i > 1 & n % i != 0)
+                        {
+                            
+                        }
+                    }
+                }                
+            }            
+        }
+    }
+}

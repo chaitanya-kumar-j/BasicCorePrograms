ed2f3cdd4e2cf97a591739d635d8b946eb4e7d57
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-24 08:04:30 +0530

    [Chaitanya] Refactor : Refactored Leap year checking Program

--- BasicPrograms/LeapYear.cs
+++ BasicPrograms/LeapYear.cs
@@ -7,37 +7,21 @@ namespace BasicPrograms
     class LeapYear
     {
         // initialize constants
-        int NUMBER_OF_DIGITS = 6;
-
-        // Randome year generator
-        Random random = new Random();
+        int NUMBER_OF_DIGITS = 4;
 
         // Check leap year or not
         public void Check()
         {
-            // random year creation
-            int testYear = random.Next(1000,1000000);
+            // random year creation by user
+            Console.WriteLine($"Enter a {NUMBER_OF_DIGITS} digit year");
+            int testYear = Convert.ToInt32(Console.ReadLine());
 
             // check whether the test year has 4 digits and is leap year or not
             if (((testYear >= Math.Pow(10, NUMBER_OF_DIGITS-1)) && (testYear < Math.Pow(10,NUMBER_OF_DIGITS))))
             {
-                if (testYear % 4 == 0) 
+                if (testYear % 4 == 0 && testYear % 100 == 0 || testYear % 400 == 0)
                 {
-                    if (testYear % 100 == 0)
-                    {
-                        if (testYear % 400 == 0)
-                        {
-                            Console.WriteLine($"{testYear} is a leap year.");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{testYear} is not a leap year.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{testYear} is a leap year.");
-                    }
+                    Console.WriteLine($"{testYear} is a leap year.");
                 }
                 else
                 {
